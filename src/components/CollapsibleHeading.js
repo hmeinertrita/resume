13cc2522b05b6a357541afc81643ff3469f3cecb
hmeinertrita <28947881+hmeinertrita@users.noreply.github.com>
2026-03-04 21:32:49 -0500

    adjust text alignment

--- src/components/CollapsibleHeading.js
+++ src/components/CollapsibleHeading.js
@@ -3,7 +3,7 @@ import Icon from "./Icon"
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({collapsed, setCollapsed, heading}) => (<>
 	<button className={collapsed ? ' collapsed' : ''} onClick={() => {setCollapsed(!collapsed)}}>
-		<h1 className={'work--heading heading--body'}>
+		<h1 className={'heading--body'}>
 			{heading}
 			<Icon icon={`arrow-${collapsed ? 'down' : 'up'}-s`} line/>
 		</h1>
